e0e17b1f799cabd67afa9b94755013a47ed911e8
Alexander <>
2021-03-15 20:10:30 +0300

    20210315

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var cheerio = require('cheerio');
 var resolve = require('url').resolve;
 var fs = require('fs');
 
-var URL = 'https://hh.ru/search/resume?clusters=True&area=1&ored_clusters=True&order_by=relevance&logic=normal&pos=full_text&exp_period=all_time&st=resumeSearch&text=nuxtjs';
+var URL = 'https://hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=nuxt';
 var results = [];
 var res_skills = [];
 var link_crawler = [];
@@ -25,12 +25,13 @@ var q = tress(function(url, callback){
         var $ = cheerio.load(res.body)
 
         //* находим в res.body ссылки на вложенные страницы -> пушим их в очередь q
-        var nextPage = $('a.resume-search-item__name')
-        
+        var nextPage = $('.bloko-link.HH-LinkModifier.HH-VacancyActivityAnalytics-Vacancy')
+        console.log(nextPage.length)
+
         //* если ссылки для переходов найдены ->
         if ( nextPage.length > 0 ) {
             nextPage.each(function(i, item) {
-                console.log('**********', item )
+                // console.log('**********', item )
                 link_crawler.push( item.children[0].data )
                 // link_crawler.push( { href: item.attribs.href, title: item.children[0].data, } )
                 q.push(resolve(URL, $(this).attr('href')))  // приводим относительный адрес ссылки к абсолютному
@@ -40,11 +41,37 @@ var q = tress(function(url, callback){
             })()
         }
 
+        //* пагинация
+        // var next_pagg = $('.bloko-gap > .bloko-button')
+        // console.log('next_pagg ', next_pagg.length)
+
+        //*  
+        // if ( next_pagg.length > 0 ) {
+        //     nextPage.each(function(i, item) {
+        //         // console.log('**********', item )
+        //         link_crawler.push( item.children[0].data )
+        //         // link_crawler.push( { href: item.attribs.href, title: item.children[0].data, } )
+        //         q.push(resolve(URL, $(this).attr('href')))  // приводим относительный адрес ссылки к абсолютному
+        //     });
+        //     (function(){
+        //         require('fs').writeFileSync('./link_crawler.json', JSON.stringify(link_crawler, null, 4));
+        //     })()
+        // }
+
+
+
+
+
+
+
         //* ищем ключевые навыки
         var skills = $('.bloko-tag__section')
         if ( skills.length > 0 ) {
+
+            //* если мы на странице резюме - записываем заголовок
+            $('.vacancy-title').text()
             skills.each(function(i, item) {
-                console.log('**********', item )
+                // console.log('**********', item )
                 res_skills.push( item.children[0].data )
                 // q.push(resolve(URL, $(this).attr('href')))  // приводим относительный адрес ссылки к абсолютному
             });
